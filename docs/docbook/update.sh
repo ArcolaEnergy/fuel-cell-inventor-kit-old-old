7962c45fe263405a7fdf84a0ac1a6299374bcdeb
Matt Venn <matt@mattvenn.net>
2012-04-18 09:06:21 +0100

    creates pdf too

--- docs/docbook/update.sh
+++ docs/docbook/update.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+
+#update html
 xsltproc --xinclude  --output ../3w.html  html.xsl 3w.xml
+#prepare fo
+xsltproc --xinclude --output 3w.fo fo.xsl 3w.xml 
+#create the pdf
+~/Downloads/fop-1.0/fop 3w.fo 3w.pdf

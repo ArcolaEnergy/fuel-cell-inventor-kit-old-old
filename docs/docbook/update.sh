c546215b7cab30ebb9358ba56707c0f00774cf7f
Matt Venn <matt@mattvenn.net>
2012-04-18 09:07:22 +0100

    create in parent dir

--- docs/docbook/update.sh
+++ docs/docbook/update.sh
@@ -5,4 +5,4 @@ xsltproc --xinclude  --output ../3w.html  html.xsl 3w.xml
 #prepare fo
 xsltproc --xinclude --output 3w.fo fo.xsl 3w.xml 
 #create the pdf
-~/Downloads/fop-1.0/fop 3w.fo 3w.pdf
+~/Downloads/fop-1.0/fop 3w.fo ../3w.pdf
